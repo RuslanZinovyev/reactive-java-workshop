97aa361262d237da23634e7108c8ba5826d40f0b
rzinovyev <ruslan.zinovyev@hotmail.com>
2023-05-31 21:43:51 -0400

    exs3 for reactive programming

--- src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise3.java
@@ -1,6 +1,7 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Exercise3 {
 
@@ -11,10 +12,9 @@ public class Exercise3 {
         // Get all numbers in the ReactiveSources.intNumbersFlux stream
         // into a List and print the list and its size
         // TODO: Write code here
-
-
-        System.out.println("Press a key to end");
-        System.in.read();
+        List<Integer> integers = ReactiveSources.intNumbersFlux().toStream().toList();
+        System.out.println("Here is the list: " + integers);
+        System.out.println("Size is: " + integers.size());
     }
 
 }
